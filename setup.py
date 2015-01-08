0db14fcae48ff286e564727244869477eb5fbce6
Matthew Peters <matt@moz.com>
2015-01-08 11:49:16 -0800

    Empty repo

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='qdr',
+    version='0.0',
+    description='Query-Document Relevance',
+    author='Moz Data Science',
+    packages=['qdr'],
+    package_dir={'qdr': 'qdr'},
+)
+
